c0e174fcfdb8ef2c235512f25b863a9ac633be56
Jake Hamilton <jehamilton122@gmail.com>
2018-06-16 20:19:42 -0700

    feat(core): Create TypeScript starter.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
-import add from './lib/add';
+import * as path from 'path';
+import * as inquirer from 'inquirer';
+import questions from './questions';
+import render from 'render-in-place';
 
-export { add };
-export default add;
+export default async (where: string) => {
+    const answers = await inquirer.prompt(questions);
+
+    const files = {
+        docs: {
+            readme: path.resolve(where, 'docs', 'README.md'),
+        },
+        readme: path.resolve(where, 'README.md'),
+        package: path.resolve(where, 'package.json'),
+    };
+
+    render(files.docs.readme, answers);
+    render(files.readme, answers);
+
+    render(files.package, answers);
+};
